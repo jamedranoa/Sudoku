c4ac50941ef41b93cf615a7d2bf4b773b2fdb215
App Academy Student <student@appacademy.io>
2014-09-11 00:37:55 -0700

    refactor code

---
+++ UI.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  console.log($("#1"))
+  currentNum = 1;
+  board = new Sudoku.Board()
+  temp = ""
+   
+   $(".buttons > ul > li").on("click",function (event) {
+     $(".buttons > ul >li").removeClass("selected")
+     currentNum = parseInt($(event.currentTarget).text())
+     $(event.currentTarget).addClass("selected")
+   });
+   
+   $(".board > ul > li").on("click", function (event) {
+     temp = currentNum
+   })
+   
+   $(".board > ul > li").on("mouseover", function (event) {
+     var $sqr = $(event.currentTarget)
+     temp = $(event.currentTarget).html()
+     board.move($sqr.data("col"),$sqr.data("row"),currentNum)
+   })
+   
+   $(".board > ul > li").on("mouseout", function (event) {
+     var $sqr = $(event.currentTarget)
+     board.move($sqr.data("col"),$sqr.data("row"),parseInt(temp))
+   })
+ })
